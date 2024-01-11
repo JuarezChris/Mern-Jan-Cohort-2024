af1db2227b970a8ee1de40fa551eed83601ef64a
Christopher Juarez <juarez.christopher@yahoo.com>
2024-01-10 20:14:05 -0600

    second commit

---
+++ week1/day2/oh/index.js
@@ -0,0 +1,12 @@
+console.log("Hello")
+
+const palidrome = (event, id) => {
+    event.preventDefault()
+    console.log(id)
+    return word.split("").reverse().join("") == word
+}
+
+function submit(e){
+    e.preventDefault()
+    console.log("hello")
+}
