57311c83b9c36814e99f7073755492fe4f4d2c95
Christopher Juarez <juarez.christopher@yahoo.com>
2024-01-18 20:47:30 -0600

    W2D3

---
+++ week2/day2/OH/client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Form from './components/Form'
+import Result from './components/Result'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Form />
+      <Result />
+    </>
+  )
+}
+
+export default App
